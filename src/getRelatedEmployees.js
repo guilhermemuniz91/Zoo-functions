d224fd86ade8ba430c9a25ed4e6a60c6efe27e51
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-10 16:12:24 -0300

    requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const employees = data.employees;
+
 const isManager = (id) => {
-  // seu código aqui
+  return employees.some(({ managers }) => id === managers.find((manager) => manager === id));
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const subordinates = employees.filter(({ managers }) => managerId === managers.find((manager) => manager === managerId));
+    return subordinates.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
