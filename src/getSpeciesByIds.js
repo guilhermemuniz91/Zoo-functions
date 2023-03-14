0eabd1e9d90f2b524cf21c61c0ad0229381e43ff
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-14 16:12:35 -0300

    correção no requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,16 +1,14 @@
 const data = require('../data/zoo_data');
 
-const species = data;
+const { species } = data;
 
 const getSpeciesByIds = (...osIds) => {
   if (osIds.length === 0) {
     return [];
   }
-  return species
-    .filter((umaEspecie) => umaEspecie.id === osIds
-      .find((umId) => umId === umaEspecie.id));
+  // const findSpeciesbyId = osIds.find((umId) => umId === )
+  // const speciesFilteredById = species.filter((umaEspecie) => )
+  return species.filter((umaEspecie) => umaEspecie.id === osIds.find((umId) => umId === umaEspecie.id));
 };
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
