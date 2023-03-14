c8b40ebd54f3a82d85c33980c797a08220cb1e0c
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-14 16:13:11 -0300

    correção eslint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,9 +6,9 @@ const getSpeciesByIds = (...osIds) => {
   if (osIds.length === 0) {
     return [];
   }
-  // const findSpeciesbyId = osIds.find((umId) => umId === )
-  // const speciesFilteredById = species.filter((umaEspecie) => )
-  return species.filter((umaEspecie) => umaEspecie.id === osIds.find((umId) => umId === umaEspecie.id));
+  return species
+    .filter((umaEspecie) => umaEspecie.id === osIds
+      .find((umId) => umId === umaEspecie.id));
 };
 
 module.exports = getSpeciesByIds;
