bde4ad4bfe6389495d18b537cac7019308d00c64
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-27 23:15:06 -0300

    correções eslint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,20 +1,7 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
+// const { species } = data;
 
-const countAnimals = (animal) => {
-  
-  // const mappedAnimals = species.map(({ name, residents }) => ({ [name]: residents.length }));
-  // const reducedAnimals = mappedAnimals.reduce((accumulator, current) => Object.assign(accumulator, current));
-  // const residentsBySpecies = species.map(({ name, residents }) => ({ specie: name, residents }));
-  // if (!animal) {
-  //   return reducedAnimals;
-  // } else if (animal.specie && !animal.sex) {
-  //   return residentsBySpecies.find(({ especie }) => especie === animal.specie.residents.length);
-  // } else {
-  //   return residentsBySpecies.find(({ especie }) => especie === animal.specie).residents
-  //   .filter((sex) => sex === animal.sex).length;
-  // }
-}
+const countAnimals = (animal) => {};
 
 module.exports = countAnimals;
