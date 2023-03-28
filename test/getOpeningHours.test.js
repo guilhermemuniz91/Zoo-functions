cac2298aac3431701e874499a85890edd7218b78
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-27 23:14:14 -0300

    correção no requisito 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,7 +1,7 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
-  it('Teste não passando argumentos deve retornar o seguinte objeto', () => {
+  it('Teste não passando argumentos deverá retornar o objeto', () => {
     expect(getOpeningHours()).toEqual({
       Tuesday: { open: 8, close: 6 },
       Wednesday: { open: 8, close: 6 },
@@ -32,5 +32,11 @@ describe('Testes da função getOpeningHours', () => {
   });
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: The minutes should represent a number', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+  });
+  it('Para os argumentos Saturday e 15:00-PM, a função deve lançar uma exceção com a mensagem: The hour must be between 0 and 12', () => {
+    expect(() => getOpeningHours('Saturday', '15:00-PM')).toThrow(/^The hour must be between 0 and 12$/);
+  });
+  it('Para os argumentos Tuesday e 09:70-AM, a função deve lançar uma exceção com a mensagem: The minutes must be between 0 and 59', () => {
+    expect(() => getOpeningHours('Tuesday', '09:70-AM')).toThrow(new Error('The minutes must be between 0 and 59'));
   });
 });
