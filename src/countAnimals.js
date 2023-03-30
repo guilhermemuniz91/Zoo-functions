687bc4b834684e1f1f1d0bf71ea19fe2e611b665
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-29 23:51:39 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
 
-const countAnimals = (animal) => {};
-
+const countAnimals = (animal) => {
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find(({ name }) => name === animal.species).residents.length;
+  }
+  return species.find((oneSpecie) => oneSpecie.name === animal.species).residents
+    .filter((oneResident) => oneResident.sex === animal.sex).length;
+};
 module.exports = countAnimals;
