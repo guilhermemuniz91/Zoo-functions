2bd0c67473e1d201769b4b5b69e32869f0309a85
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-30 09:13:38 -0300

    requisito 11 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { employees, species } = data;
+
+const AllEmployees = () => employees.map((oneEmployee) => ({
+  id: oneEmployee.id,
+  fullName: `${oneEmployee.firstName} ${oneEmployee.lastName}`,
+  species: oneEmployee.responsibleFor.map((specieId) => data.species
+    .find(({ id }) => id === specieId).name),
+  locations: oneEmployee.responsibleFor.map((specieId) => data.species
+    .find(({ id }) => id === specieId).location),
+}));
+
+const singleEmployee = (oneEmployee) => ({
+  id: oneEmployee.id,
+  fullName: `${oneEmployee.firstName} ${oneEmployee.lastName}`,
+  species: oneEmployee.responsibleFor.map((specieId) => species
+    .find(({ id }) => id === specieId).name),
+  locations: oneEmployee.responsibleFor.map((specieId) => species
+    .find(({ id }) => id === specieId).location),
+});
+
+const findEmployee = (param) => {
+  const getEmployee = employees.find(({ id, firstName, lastName }) => id === param.id
+    || firstName === param.name || lastName === param.name);
+  if (!getEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  return getEmployee;
+};
+
+const getEmployeesCoverage = (param) => {
+  if (!param) { return AllEmployees(); }
+  return singleEmployee(findEmployee(param));
 };
 
 module.exports = getEmployeesCoverage;
